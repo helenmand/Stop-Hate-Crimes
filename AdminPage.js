49670fe7b049f21b12a9e8fa84ecdf2fb4c8a51e
AngelopoulosD <dpangelo@csd.auth.gr>
2022-04-26 00:04:30 +0300

    Manage update buttons

--- AdminPage.js
+++ AdminPage.js
@@ -4,5 +4,27 @@ function getAdminName()
 	document.getElementById("AdminName").innerHTML=Username;
 }
 
-
-
+function UpdateButtonUsers()
+{
+	username=document.getElementById("Username").value;
+	if (username=="NotExist")
+	{
+		document.getElementById("WarningUsername").innerHTML="User Not Exist";
+	}
+}
+function UpdateButtonComplains()
+{
+	complainid=document.getElementById("ComplainID").value;
+	if (complainid=="NotExist")
+	{
+		document.getElementById("WarningComplain").innerHTML="Complain Not Found";
+	}
+}
+function UpdateButtonArticle()
+{
+	articleid=document.getElementById("ArticleID").value;
+	if (articleid=="NotExist")
+	{
+		document.getElementById("WarningArticle").innerHTML="Article Not Found";
+	}
+}
