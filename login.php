095c9c9e49dbe6c8c5b4bce2e9f9bd74859b3cb2
AngelopoulosD <dpangelo@csd.auth.gr>
2022-05-26 11:27:43 +0300

    added a check to show warning for username or password is incorrect

--- login.php
+++ login.php
@@ -10,25 +10,31 @@
 		<div>
             <?php include "header.php";?>
         </div>
-		<h1 class="headerlogin">Log in</h1> 
-		<div class="gridcontainer">
-			<div class="grid-item">
-				<label class="labelsposition" for="usname">Username</label>
-			</div>
-			<div class="grid-tem">
-				<input type="text" id="usname" name="username">
-			</div>
-			<div class="grid-item">
-				<label class="labelsposition" for="psword">Password&nbsp;</label>
-			</div>
-			<div class="grid-item">
-				<input type="password" id="psword" name="password"><br>
-				<label class="warningLabel" id="warningOutput"></label>
-			</div>
-		</div>
-		<div class="loginButtonPosition">
-			<input class="loginButton" type="submit" onclick="checkinput()" value="Log in">
-		</div>
-	
+        <form  action="./loginAuthentication.php" method="post">
+            <h1 class="headerlogin">Log in</h1>
+            <div class="gridcontainer">
+                <div class="grid-item">
+                    <label class="labelsposition" for="usname">Username</label>
+                </div>
+                <div class="grid-tem">
+                    <input type="text" id="usname" name="username">
+                </div>
+                <div class="grid-item">
+                    <label class="labelsposition" for="psword">Password&nbsp;</label>
+                </div>
+                <div class="grid-item">
+                    <input type="password" id="psword" name="password"><br>
+                    <?php
+                        if(isset($_GET["error"])){
+                            echo "<label class='warningLabel' id='warningOutput'>The username or password is incorrect</label>";
+                        }
+                    ?>
+
+                </div>
+            </div>
+            <div class="loginButtonPosition">
+                <input class="loginButton" type="submit" value="Log in">
+            </div>
+        </form>
 	</body>
 </html> 
