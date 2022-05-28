c2cac82fccbfeda3be323276f5da70523895343b
Christos Apostolidis <chriapos@csd.auth.gr>
2022-05-29 00:25:29 +0300

    make submission works!

---
+++ makeSubmission.js
@@ -0,0 +1,9 @@
+function hideRegion() {
+    document.getElementById("postRegion").style.visibility = "hidden";
+    document.getElementById("postRegionLabel").style.visibility = "hidden";
+}
+
+function showRegion() {
+    document.getElementById("postRegion").style.visibility = "visible";
+    document.getElementById("postRegionLabel").style.visibility = "visible";
+}
--- makeSubmission.php
+++ makeSubmission.php
@@ -2,25 +2,28 @@
 <html>
     <head>
         <link rel="stylesheet" href="makeSubmission.css">
+        <script src="makeSubmission.js"></script>
     </head>
     <body>
         <div>
             <?php include "header.php";?>
         </div>
-        <form id="postForm">
+        <form id="postForm" action="processPostSubmission.php" method="post">
             <div>
-                <input type="radio" id="post" name="postType" value="post">
-                <label for="post">Post</label>
+                <input type="radio" id="Article" name="postType" value="Article" onchange="hideRegion();">
+                <label for="Article">Article</label>
             </div>
             <div>
-                <input type="radio" id="report" name="postType" value="report">
-                <label for="report">Report</label>
+                <input type="radio" id="Complaint" name="postType" value="Complaint" onchange="showRegion();">
+                <label for="Complaint">Complaint</label>
             </div>
 
             <label for="postTitle">Title:</label>
             <input type="text" id="postTitle" name="postTitle">
             <label for="postBody">Body:</label>
             <textarea form_id="postForm" type="text" id="postBody" name="postBody"> </textarea>
+            <label for="postRegion" id="postRegionLabel">Region:</label>
+            <input type="text" id="postRegion" name="postRegion">
             <input type="submit" value="Submit" id="formSubmitButton">
         </form>
     </body>
