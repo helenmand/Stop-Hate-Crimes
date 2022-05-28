8890b590c18cad58d670a42d5c7107679e8566ec
AngelopoulosD <dpangelo@csd.auth.gr>
2022-05-28 21:26:48 +0300

    update an account from user, admin to user and admin to admin

--- AdminsInputCheck.php
+++ AdminsInputCheck.php
@@ -1,29 +1,29 @@
 <?php
     include("AdminsConnection.php");
+    $username=$_POST['username'];
 
-    if($_POST['userbuttons']=="Update"){
-        $username=$_POST['username'];
+    $sql="SELECT `USERNAME`, `PASSWORD`, `EMAIL` FROM users WHERE `USERNAME`='".$username."';";
 
-        $sql="SELECT `USERNAME`, `PASSWORD`, `EMAIL` FROM users WHERE `USERNAME`='".$username."';";
+    $results = mysqli_query($conn, $sql);
+    $user = mysqli_fetch_array($results);
 
-        $results = mysqli_query($conn, $sql);
-        $user = mysqli_fetch_array($results);
-
-        if(isset($user)){
-            session_start();
+    if(isset($user)){
+    	if($_POST['userbuttons']=="Update"){
+    		session_start();
             $_SESSION['us_pw_em']=$user;
             header("Location:./account-settings.php");
-        }
-        else{
-            $error="1";
-            header("Location:./AdminPage.php?error=".$error);
-        }
+    	}
+    	else{
+			$sql="DELETE FROM `users` WHERE `USERNAME`='".$username."';";
+
+		    if ($conn->query($sql) === TRUE) {
+		        header("Location:./AdminPage.php");
+		    }
+    	}
 
     }
-
-
-
-
-
-
+    else{
+            $error="1";
+            header("Location:./AdminPage.php?error=".$error);
+    }
 ?>
