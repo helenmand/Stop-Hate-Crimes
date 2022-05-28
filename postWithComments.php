4f6dd1f7b76a94cea6f868756b8e88ac621c0790
Christos Apostolidis <chriapos@csd.auth.gr>
2022-05-28 21:54:40 +0300

    removed view comments if comments are being viewed

--- postWithComments.php
+++ postWithComments.php
@@ -24,7 +24,27 @@
                     $titletext = $row["TITLE"];
                     $bodytext = $row["CONTENT"];
                     $postid = $row["ID"];
-                    include("post.php");
+                    echo
+                    "<div class=\"post\">
+                        <div class=\"postUser\">
+                            <a class=\"postUserLink\" href=\"profilepage.php?user=".$usertext."\">
+                                ".$usertext."
+                            </a>
+                        </div>
+                        <div class=\"postTitle\">
+                            <a>".$titletext."</a>
+                        </div>
+                        <div class=\"postBody\">
+                            <a>
+                                ".$bodytext."
+                            </a>
+                        </div>
+                        <div class=\"postButtons\">
+                            <button class=\"postReplyButton bn1\" onclick=\"openReply(".$postid.");\">
+                                Reply
+                            </button>
+                        </div>
+                    </div>";
                 ?>
             </div>
             <div id="commentList">
