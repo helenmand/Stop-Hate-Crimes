e377d5f7570b1d6d58af578d56f54b68a1df415b
Zoi Ntaouti <ntaoutiz@csd.auth.gr>
2022-05-29 00:36:48 +0300

    comment on comment creation

--- commentSubmission.php
+++ commentSubmission.php
@@ -1,3 +1,7 @@
+<!DOCTYPE html>
+<html>
+<head></head>
+<body>
 <?php
    
    
@@ -10,18 +14,31 @@
             die("Connection failed: " . $conn->connect_error);
         }
 
-        $replyid = mysqli_real_escape_string($conn, $_GET["postid"]);
         $username = mysqli_real_escape_string($conn, $_COOKIE['user']);
         $comment = mysqli_real_escape_string($conn, $_POST['postBody']);
+        $postid = mysqli_real_escape_string($conn, $_GET["postid"]);
+
         $query = "SELECT MAX(ID) + 1 FROM comments;";
         $id = mysqli_query($conn, $query);
         $row = mysqli_fetch_array($id);
-        $sql="INSERT INTO `comments` (`ID`, `USERNAME`, `COMMENT`, `ARTICLE_ID`) VALUES ('".$row["MAX(ID) + 1"]."', '".$username."', '".$comment."', '".$replyid."');";
+
+        $sql="INSERT INTO `comments` (`ID`, `USERNAME`, `COMMENT`, `ARTICLE_ID`) VALUES ('".$row["MAX(ID) + 1"]."', '".$username."', '".$comment."', '".$postid."');";
         mysqli_query($conn, $sql);
-        header("Location:./postWithComments.php?postid=".$replyid);
+
+
+        if(isset($_GET["commentid"])){
+            $replyid = mysqli_real_escape_string($conn, $_GET["commentid"]);
+
+            $sql="INSERT INTO `comments_on_comments` (`COMMENTS`, `FOLLOWS`) VALUES ('".$replyid."', '".$row["MAX(ID) + 1"]."');";
+            mysqli_query($conn, $sql);
+        }
+
+        header("Location:./postWithComments.php?postid=".$postid);
     }
     else{
         echo "You are not logged in";
     }
 
 ?>
+</body>
+</html>
