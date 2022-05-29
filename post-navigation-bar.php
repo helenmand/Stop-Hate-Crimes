a9bfae9fddfafd33052a27efcf1b9309b665f3a1
Christos Apostolidis <chriapos@csd.auth.gr>
2022-05-29 16:52:31 +0300

    new and improved pvb regions

--- post-navigation-bar.php
+++ post-navigation-bar.php
@@ -7,7 +7,7 @@
     <body>
         <?php 
             function redirectRegion ($region) {
-                $currentUrl = substr($_SERVER['REQUEST_URI'], 0, strpos($_SERVER['REQUEST_URI'], "?"))."?pvbregion=".$region;
+                $currentUrl = substr($_SERVER['REQUEST_URI'], 0, strpos($_SERVER['REQUEST_URI'], "?"))."?pvbregion=".str_replace(" ","_",$region);
                 if (isset($_GET["pvbcontent"])) {
                     $currentUrl.="&pvbcontent=".$_GET["pvbcontent"];
                 }
@@ -40,7 +40,7 @@
                     <?php
                     
                         if(isset($_GET["pvbregion"])) {
-                            echo $_GET["pvbregion"];
+                            echo str_replace("_"," ",$_GET["pvbregion"]);
                         } else {
                             echo "Everywhere";
                         }
@@ -48,11 +48,19 @@
                     ?>
                 </a>
                 <div class="dropdown-content">
-                    <a href=<?php echo "\"".redirectRegion("Everywhere")."\"" ?>>Everywhere</a>
-                    <a href=<?php echo "\"".redirectRegion("Thessaloniki")."\"" ?>>Thessaloniki</a>
-                    <a href=<?php echo "\"".redirectRegion("Athens")."\"" ?>>Athens</a>
-                    <a href=<?php echo "\"".redirectRegion("Larissa")."\"" ?>>Larissa</a>
-                    <a href=<?php echo "\"".redirectRegion("Serres")."\"" ?>>Serres</a>
+                    <?php 
+
+                        $conn = @mysqli_connect("localhost", "NotLoginUsers", "NLogUsSHC");
+                        @mysqli_select_db($conn, "STOP_HATE_CRIMES_DB");
+                        $query = "SELECT DISTINCT region FROM complaints";
+                        $results = mysqli_query($conn, $query);
+                        echo "<a href=".redirectRegion("Everywhere").">Everywhere</a>";
+                        while($row = mysqli_fetch_array($results)) {
+                            
+                            echo "<a href=".redirectRegion($row["region"]).">".$row["region"]."</a>";
+                        }
+
+                    ?>
                 </div>
             </div>
             <div class="buttons-right">
