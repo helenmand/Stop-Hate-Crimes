454c35ee32c809d58441e7f5c99a222cc7951c80
AngelopoulosD <dpangelo@csd.auth.gr>
2022-05-29 14:31:10 +0300

    Add Update Delete from articles complains

--- processPostSubmission.php
+++ processPostSubmission.php
@@ -1,30 +1,53 @@
 <?php
+    session_start();
 
-    $conn = @mysqli_connect("localhost", "LoginUsers", "LogUsSHC");
-    @mysqli_select_db($conn, "STOP_HATE_CRIMES_DB"); 
+    if($_COOKIE['user_category']=="user" or (($_SESSION['action']!=null and $_SESSION['action']=="add"))){
 
-    $postTitle = mysqli_real_escape_string($conn, $_POST["postTitle"]);
-    $postBody = mysqli_real_escape_string($conn, $_POST["postBody"]);
-    $postRegion = mysqli_real_escape_string($conn, $_POST["postRegion"]);
+        $conn = @mysqli_connect("localhost", "LoginUsers", "LogUsSHC");
+        @mysqli_select_db($conn, "STOP_HATE_CRIMES_DB");
 
-    if(strcmp($_POST["postType"], "Article") == 0) {
+        $postTitle = mysqli_real_escape_string($conn, $_POST["postTitle"]);
+        $postBody = mysqli_real_escape_string($conn, $_POST["postBody"]);
+        $postRegion = mysqli_real_escape_string($conn, $_POST["postRegion"]);
 
-        $query = "SELECT MAX(ID) + 1 FROM articles;";
-        $results = mysqli_query($conn, $query);
-        $row = mysqli_fetch_array($results);
+        if(strcmp($_POST["postType"], "Article") == 0) {
 
-        $query = "INSERT INTO `articles` (`ID`, `TITLE`, `USERNAME`, `CONTENT`, `DATE`) VALUES ('".$row["MAX(ID) + 1"]."', '".$postTitle."', '".$_COOKIE["user"]."', '".$postBody."', '".date('y-m-d h:i:s')."');";
-        mysqli_query($conn, $query);
-    } else if(strcmp($_POST["postType"], "Complaint") == 0) {
+            $query = "SELECT MAX(ID) + 1 FROM articles;";
+            $results = mysqli_query($conn, $query);
+            $row = mysqli_fetch_array($results);
 
-        $query = "SELECT MAX(ID) + 1 FROM COMPLAINTS;";
-        $results = mysqli_query($conn, $query);
-        $row = mysqli_fetch_array($results);
-        
+            $query = "INSERT INTO `articles` (`ID`, `TITLE`, `USERNAME`, `CONTENT`, `DATE`) VALUES ('".$row["MAX(ID) + 1"]."', '".$postTitle."', '".$_COOKIE["user"]."', '".$postBody."', '".date('y-m-d h:i:s')."');";
+            mysqli_query($conn, $query);
+        } else if(strcmp($_POST["postType"], "Complaint") == 0) {
 
-        $query = "INSERT INTO `complaints` (`ID`, `TITLE`, `USERNAME`, `CONTENT`, `REGION`, `DATE`) VALUES ('".$row["MAX(ID) + 1"]."', '".$postTitle."',  '".$_COOKIE["user"]."','".$postBody."', '".$postRegion."', '".date('y-m-d h:i:s')."');";
-        mysqli_query($conn, $query);
+            $query = "SELECT MAX(ID) + 1 FROM `complaints`;";
+            $results = mysqli_query($conn, $query);
+            $row = mysqli_fetch_array($results);
+
+            $query = "INSERT INTO `complaints` (`ID`, `TITLE`, `USERNAME`, `CONTENT`, `REGION`, `DATE`) VALUES ('".$row["MAX(ID) + 1"]."', '".$postTitle."',  '".$_COOKIE["user"]."','".$postBody."', '".$postRegion."', '".date('y-m-d h:i:s')."');";
+            mysqli_query($conn, $query);
+        }
     }
-    header("Location:./feed.php");
+    else{
+        include("AdminsConnection.php");
 
+        $postTitle = mysqli_real_escape_string($conn, $_POST["postTitle"]);
+
+        if($_SESSION['post']=="complaint"){
+
+            $sql="UPDATE `complaints` SET `TITLE`='".$postTitle."',`CONTENT`='".$_POST["postBody"]."',`REGION`='".$_POST["postRegion"]."' WHERE `ID`='".$_SESSION['data']['ID']."';";
+        }
+        else{
+            $sql="UPDATE `articles` SET `TITLE`='".$postTitle."',`CONTENT`='".$_POST["postBody"]."' WHERE `ID`='".$_SESSION['data']['ID']."';";
+        }
+
+        $conn->query($sql);
+    }
+
+    if($_COOKIE['user_category']=="user"){
+        header("Location:./feed.php");
+    }
+    else{
+        header("Location:./AdminPage.php");
+    }
 ?>
