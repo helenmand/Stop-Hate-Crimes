4ce25f1deb3f847b7cad5855c1fd808d1bce8527
Christos Apostolidis <chriapos@csd.auth.gr>
2022-05-30 16:51:38 +0300

    disables required attribute on region when article

--- makeSubmission.js
+++ makeSubmission.js
@@ -1,9 +1,11 @@
 function hideRegion() {
     document.getElementById("postRegion").style.visibility = "hidden";
     document.getElementById("postRegionLabel").style.visibility = "hidden";
+    document.getElementById("postRegion").removeAttribute("required");
 }
 
 function showRegion() {
     document.getElementById("postRegion").style.visibility = "visible";
     document.getElementById("postRegionLabel").style.visibility = "visible";
+    document.getElementById("postRegion").required = true;
 }
