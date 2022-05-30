d2b9d78893a6f88c0b5c3b3019526e0aae9363f4
Christos Apostolidis <chriapos@csd.auth.gr>
2022-05-30 16:37:01 +0300

    fixed bug

--- searchResult.php
+++ searchResult.php
@@ -74,7 +74,7 @@
                         }
 
                     } else if(strcmp($pvbcontent, "Articles") == 0) {
-                        $sqlQuery = "SELECT * FROM articles WHERE TITLE LIKE \"%".$_GET["q"]."%\" OR content LIKE \"%".$_GET["q"]."%\" ORDER BY articles.date LIMIT 10";
+                        $sqlQuery = "SELECT * FROM articles WHERE TITLE LIKE \"%".$_GET["q"]."%\" OR content LIKE \"%".$_GET["q"]."%\" ORDER BY articles.date DESC LIMIT 10";
                         $results = mysqli_query($conn, $sqlQuery);
                         while($row = mysqli_fetch_array($results)) {
                             $usertext = $row["USERNAME"];
@@ -85,9 +85,9 @@
                         }
                     } else if(strcmp($pvbcontent, "Complaints") == 0) {
                         if(strcmp($pvbregion, "Everywhere") == 0) {
-                            $sqlQuery = "SELECT * FROM complaints WHERE TITLE LIKE \"%".$_GET["q"]."%\" OR content LIKE \"%".$_GET["q"]."%\" ORDER BY complaints.date LIMIT 10"; 
+                            $sqlQuery = "SELECT * FROM complaints WHERE TITLE LIKE \"%".$_GET["q"]."%\" OR content LIKE \"%".$_GET["q"]."%\" ORDER BY complaints.date DESC LIMIT 10"; 
                         } else {
-                            $sqlQuery = "SELECT * FROM complaints WHERE region LIKE \"".$pvbregion."\" AND (TITLE LIKE \"%".$_GET["q"]."%\"  ORDER BY complaints.date OR content LIKE \"%".$_GET["q"]."%\") LIMIT 10";
+                            $sqlQuery = "SELECT * FROM complaints WHERE region LIKE \"".$pvbregion."\" AND (TITLE LIKE \"%".$_GET["q"]."%\" OR content LIKE \"%".$_GET["q"]."%\")  ORDER BY complaints.date DESC LIMIT 10";
                         }
                         
                         $results = mysqli_query($conn, $sqlQuery);
